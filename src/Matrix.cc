b9d61d23cc38a2f382e4b9dc443125562a7afe7c
John Ludwig <john@theludwigs.com>
2014-05-11 20:25:58 -0700

    correct in-place conversion bug

--- src/Matrix.cc
+++ src/Matrix.cc
@@ -1492,6 +1492,7 @@ Matrix::CvtColor(const v8::Arguments& args) {
     HandleScope scope;
 
     Matrix * self = ObjectWrap::Unwrap<Matrix>(args.This());
+    cv::Mat res;
 
     v8::String::Utf8Value str (args[0]->ToString());
     std::string str2 = std::string(*str);
@@ -1529,7 +1530,10 @@ Matrix::CvtColor(const v8::Arguments& args) {
 			"Conversion code is unsupported")));
     }
 
-    cv::cvtColor(self->mat, self->mat, iTransform);
+    cv::cvtColor(self->mat, res, iTransform);
+
+
+    res.copyTo(self->mat);
 
     return scope.Close(Undefined());
 }
